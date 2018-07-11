010789a4bee2066d4692d55568b82e0717dcd1d0
Sasa <dobricceltic5@gmail.com>
2018-07-11 18:20:32 +0200

    Dodati fajlovi koji nedostaju.

---
+++ poslovna/src/main/java/poslovna/informatika/poslovna/repository/PoslovnaGodinaRepository.java
@@ -0,0 +1,12 @@
+package poslovna.informatika.poslovna.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import poslovna.informatika.poslovna.model.PoslovnaGodina;
+
+@Repository
+public interface PoslovnaGodinaRepository extends JpaRepository<PoslovnaGodina,Long> {
+
+		PoslovnaGodina findByZakljucena(boolean zakljucena);
+}
